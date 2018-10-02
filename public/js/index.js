8e5416f6b7da9135839c20d76dae3cefce7d248f
Mohamed Ali <mohamed.ali4@ibm.com>
2018-10-02 18:04:06 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,17 @@ socket.on('disconnect', function (message) {
 });
 
 socket.on('newMessage', function (message) {
+var formatedTime = moment(message.createdAt).format('h:mm a');
+
+var template = jQuery('#message-template').html();   //html() function returns the html which insdie this(message-template) template
+var html = Mustache.render(template, {
+  text: message.text,
+  from: message.from,
+  createdAt: formatedTime
+}); //render() function returns the actual html code which i can append to any html element, second argment is object contains the data i want to inject inside my html template
+jQuery('#messages').append(html);
+
+/*
   //i get the moment.js file from node_modules and import it in index.html file to use it inside index.js file to format my timestamps
   var formatedTime = moment(message.createdAt).format('h:mm a');
 
@@ -17,11 +28,22 @@ socket.on('newMessage', function (message) {
   li.text(`${message.from} ${formatedTime}: ${message.text} `);
 
   jQuery('#messages').append(li);
+*/
 });
 
 socket.on('newLocationMessage', function (message) {
   var formatedTime = moment(message.createdAt).format('h:mm a');
 
+  var template = jQuery('#location-message-template').html();   //html() function returns the html which insdie this(message-template) template
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formatedTime,
+    url: message.url
+  }); //render() function returns the actual html code which i can append to any html element, second argment is object contains the data i want to inject inside my html template
+  jQuery('#messages').append(html);
+
+
+/*
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
@@ -29,6 +51,7 @@ socket.on('newLocationMessage', function (message) {
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
+*/
 });
 
 jQuery('#message-form').on('submit', function (e) {
